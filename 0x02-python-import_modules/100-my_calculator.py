b32cce9a0d7cbf9661a98c32e618589cff835bcf
TonnyMpho <tonnympho1@gmail.com>
2023-06-08 14:45:24 -0700

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    op = argv[2]
+
+    if op != "+" and op != "-" and op != "*" and op != "/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    if operator == "+":
+        print("{} {} {} = {}".format(num1, op, num2, add(num1, num2)))
+    elif operator == "-":
+        print("{} {} {} = {}".format(num1, op, num2, sub(num1, num2)))
+    elif operator == "*":
+        print("{} {} {} = {}".format(num1, op, num2, mul(num1, num2)))
+    elif operator == "/":
+        print("{} {} {} = {}".format(num1, op, num2, div(num1, num2)))
