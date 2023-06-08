df682a12d7a7aff1bf3c5f35de11bd44dff5632c
TonnyMpho <tonnympho1@gmail.com>
2023-06-08 14:06:33 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__=="__main__":
 
     if len(argv) < 2:
         print("0 arguments")
-    elif len(argv) >= 2:
+    elif len(argv) == 2:
+        print("{} argument".format(len(argv) - 1))
+    else:
         print("{} arguments".format(len(argv) - 1))
 
     for i in range(1, len(argv)):
