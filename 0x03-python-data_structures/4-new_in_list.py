2a70330b50367da75c855f8a1e34553e047621f9
TonnyMpho <tonnympho1@gmail.com>
2023-06-11 12:56:47 -0700

    0x03. Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhon3
+
+def new_in_list(my_list, idx, element):
+
+    new_list = my_list
+
+    if idx < 0:
+        return new_list
+    elif idx > (len(new_list) - 1):
+        return new_list
+
+    new_list[idx] = element
+
+    return new_list
