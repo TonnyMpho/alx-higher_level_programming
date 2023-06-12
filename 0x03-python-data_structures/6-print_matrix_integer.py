fd07b74b838ada16384b803ea5944fe0243507e2
TonnyMpho <tonnympho1@gmail.com>
2023-06-12 11:40:05 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,8 @@ def print_matrix_integer(matrix=[[]]):
 
     for num in matrix:
         for i in range(len(num)):
-            print("{:d}".format(num[i]),
-            end="" if i == len(num) - 1 else " ")
+            print(
+                    "{:d}".format(num[i]),
+                    end="" if i == len(num) - 1 else " ")
 
         print()
