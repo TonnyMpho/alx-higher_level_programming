4eeaacddef81f13de0fb5461d7963ab005389343
TonnyMpho <tonnympho1@gmail.com>
2023-06-12 05:08:11 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,4 +5,4 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return (len(sentence), None)
     else:
-        return (len(sentece), sentence[0])
+        return (len(sentence), sentence[0])
