ea518aa80c2c60bf247099cd96f5cf174be53737
TonnyMpho <tonnympho1@gmail.com>
2023-06-14 09:25:14 -0700

    0x04. Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list or len(my_list) == 0:
+        return 0
+
+    sum_score = 0
+    sum_weight = 0
+
+    for score, weight in my_list:
+        sum_score += score * weight
+        sum_weight += weight
+
+    average = sum_score / sum_wight
+
+    return average
