272dcb00cf5d57c07a0aeb814db331a659cde2c5
TonnyMpho <tonnympho1@gmail.com>
2023-06-14 09:56:40 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+
+    key_del = []
+
     for key, val in a_dictionary.items():
         if val == value:
-            del a_dictionary[key]
+            key_del.append(key)
+
+    for key in key_del:
+        del a_dictionary[key]
 
     return a_dictionary
