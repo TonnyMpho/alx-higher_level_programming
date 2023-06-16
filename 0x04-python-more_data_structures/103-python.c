5e217a0611dd45b6473874828e92562d59a240c1
TonnyMpho <tonnympho1@gmail.com>
2023-06-16 13:24:27 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -55,12 +55,17 @@ void print_python_bytes(PyObject *p)
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", bytes);
 
-	printf("  first %ld bytes: ", size >= 10 ? 10 : size + 1);
+	if (size >= 10)
+		size = 10;
+	else
+		size += 1;
 
-	for (i = 0; i < (size >= 10 ? 10 : size + 1); i++)
+	printf("  first %ld bytes: ", size);
+
+	for (i = 0; i < size; i++)
 	{
 		printf("%02hhx", bytes[i]);
-		if (i == ((size > 10) ? 9 : size - 1))
+		if (i == (size - 1))
 			printf("\n");
 		else
 			printf(" ");
