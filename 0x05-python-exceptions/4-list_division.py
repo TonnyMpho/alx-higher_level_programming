a63929340ce3cfbcf632a3047c6d6979d51dea32
TonnyMpho <tonnympho1@gmail.com>
2023-06-26 11:59:21 -0700

    0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    list_results = []
+    results = 0
+
+    for i in range(list_lenght):
+        try:
+            results = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        list_results.append(results)
+
+    return (list_results)
---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    num = 3
+    name = "TM"
+    results = num + name
