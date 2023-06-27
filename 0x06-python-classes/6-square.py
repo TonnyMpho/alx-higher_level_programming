1c17800e627080473ffebb59caa311ee63265bf8
TonnyMpho <tonnympho1@gmail.com>
2023-06-27 12:53:55 -0700

    0x06. Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,4 +43,4 @@ class Square:
 
         """  prints in stdout the square with the character # """
         for i in range(self.__size):
-            print("#" * self.__size)
+            print("#" * self.__size))
