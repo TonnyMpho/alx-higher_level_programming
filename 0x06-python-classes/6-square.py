5d6ef415cd631a3e2632e4469b088082900c2ef1
TonnyMpho <tonnympho1@gmail.com>
2023-06-27 13:31:34 -0700

    0x06. Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,7 +42,7 @@ class Square:
         """
         if type(value) != tuple or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = position
+        self.__position = value
 
     """ Public instance method that returns the current square area """
     def area(self):
@@ -65,5 +65,5 @@ class Square:
         for i in range(self.__size):
             for j in range(self.__position[0]):
                 print(" ", end="")
-            [print("#", end="") for k in range(0, self.__size)]
+            [print("#", end="") for k in range(self.__size)]
         print()
