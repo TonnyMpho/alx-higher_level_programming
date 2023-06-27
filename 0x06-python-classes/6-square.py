810939583a07ab33d5ce7b59f0f6c84ca4d8cd7d
TonnyMpho <tonnympho1@gmail.com>
2023-06-27 13:24:36 -0700

    0x06. Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,15 +4,21 @@
 
 
 class Square:
-    """ Instantiation with optional size """
-    def __init__(self, size=0):
-        """ Private instance attribute size """
+    """ Instantiation with optional size and position"""
+    def __init__(self, size=0, position=(0, 0)):
+        """ Private instance attribute size and position"""
         self.__size = size
+        self.__position = position
 
     """ getter to retrieve size """
     @property
     def size(self):
         return self.__size
+
+    """ Getter to retrieve position """
+    @property
+    def position(self):
+        return self.__position
 
     """ property setter to set size """
     @size.setter
@@ -26,6 +32,17 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    """ Property setter to set position """
+    @position.setter
+    def position(self, value):
+        """
+        position must be a tuple of 2 positive integers,
+        otherwise raise a TypeError exception
+        """
+        if type(value) != tuple or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
 
     """ Public instance method that returns the current square area """
     def area(self):
@@ -42,5 +59,10 @@ class Square:
             print()
 
         """  prints in stdout the square with the character # """
+        for i in range(self.__position[1]):
+            print()
         for i in range(self.__size):
-            print("#" * self.__size))
+            for j in range(self.__position[0]):
+                print(" ", end="")
+            [print("#", end="") for k in range(self.__size)]
+        print()
