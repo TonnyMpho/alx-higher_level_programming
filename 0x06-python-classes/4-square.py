e764e19da6fd2dd186d80f3188eb4cdfe55ab0f0
TonnyMpho <tonnympho1@gmail.com>
2023-06-27 12:36:12 -0700

    0x06. Python - Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-# 3-square.py - By TM
+# 4-square.py - By TM
 """ class Square that defines a square """
 
 
 class Square:
     """ Instantiation with optional size """
     def __init__(self, size=0):
-        """ Private instance attribute size"""
+        """
+        Private instance attribute size
+
+        Checking if size is an integer or not less than 0
+        otherwise raise an Exception
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     """ getter to retrieve size """
