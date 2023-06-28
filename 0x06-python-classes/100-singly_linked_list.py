315ca209674dd6d2b8f26553e8fe30a953b3dce4
TonnyMpho <tonnympho1@gmail.com>
2023-06-27 18:26:03 -0700

    0x06. Python - Classes and Objects

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -9,22 +9,28 @@ class Node:
         self.data = data
         self.next_node = next_node
 
+        """ getter that retrive data """
         @property
         def data(self):
             return self.__data
 
+        """ property setter to set data """
         @data.setter
         def data(self, value):
+            """ check if value is an integer """
             if (not isinstance(value, int)):
                 raise ValueError("data must be an integer")
             self.__data = value
 
+        """ getter that retrive next_node """
         @property
         def next_node(self):
             return self.__next_node
 
+        """ property setter to set next_node """
         @next_node.setter
         def next_node(self, value):
+            """ check if value is Node and is None """
             if (not isinstance(value, Node) or value != None):
                 raise TypeError("next_node must be a Node object")
             self.__next_node = value
@@ -37,6 +43,7 @@ class SinglyLinkedList:
         """ Private instance attribute: head (no setter or getter) """
         self.__head = None
 
+    """ Public instance method that adds a new node in a sorted list """
     def sorted_insert(self, value):
         new_node = Node(value)
 
@@ -57,6 +64,7 @@ class SinglyLinkedList:
         new_node.next-node = curr.next_node
         curr.next_node = new_node
 
+    """ Public instance method that prints the list """
     def __str__(self):
         if self.__head is None:
             return ""
