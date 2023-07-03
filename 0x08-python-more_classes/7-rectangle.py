73e8f2c665715b08f258c41a6c17ef7d8b73a194
TonnyMpho <tonnympho1@gmail.com>
2023-07-03 12:14:13 -0700

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         for i in range(self.__height):
-            rect += self.print_symbol * self.__width
+            rect += str(self.print_symbol) * self.__width
             if i != self.__height - 1:
                 rect += "\n"
         return rect
