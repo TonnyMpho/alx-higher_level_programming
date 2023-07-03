fede0cadf61fce2451b164eea293cd084be83207
TonnyMpho <tonnympho1@gmail.com>
2023-07-03 09:02:20 -0700

    0x08. Python - More Classes and Objects

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" class Rectangle that defines a rectangle """
+
+
+class Rectangle:
+    """
+    class Rectangle:
+    Defines or represents a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """ instantiation with optional width and height """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ property width to retrieve the width """
+        return __width
+
+    @width.setter
+    def width(self, value):
+        """ property setter - width to set the width """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ property height to retrieve the width """
+        return __height
+
+    @height.setter
+    def height(self, value):
+        """
+        property setter - width to set the width
+        value passed must be an integer and >= 0
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
