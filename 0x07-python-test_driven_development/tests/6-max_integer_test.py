aa7a97f4b3de875693200fc55bee197c4081377e
TonnyMpho <tonnympho1@gmail.com>
2023-07-05 13:11:01 -0700

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,17 +6,24 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """
+    Unittesting the function 6-max_integer
+    """
 
     def empylist(self):
-        self.assertEqual(max_integer([]), None)
+        """ Testing for an empty list """
+        self.assertNone(max_integer([]), None)
 
     def findmax_ordered(self):
+        """ Testing list """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def findmax_unordered(self):
+        """ Testing an unordered list """
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
-    def findmax_begging(self):
+    def findmax_beginning(self):
+        """ Testing - max integer if its at the beginning of the list"""
         self.assertEqual(max_integer([4, 3, 1, 2]), 4)
 
 if __name__=='__main__':
