06896a7fb5e45cc7648cd71f6e3bee6da78c49ed
TonnyMpho <tonnympho1@gmail.com>
2023-07-05 14:23:23 -0700

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_string - function that prints Python strings
+ * @p: Python object
+ * Return: Void (nothing)
+ */
+void print_python_string(PyObject *p)
+{
+	py_ssize_t len;
+	char *str;
+
+	if (!PyUnicode_Check(p))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	str = PyUnicode_AsUTF8AndSize(p, &len);
+
+	printf("[.] string object info\n");
+
+	if (pyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+
+	printf("  length: %zd\n", len);
+	printf("  value: %s\n", str);
+}
+
