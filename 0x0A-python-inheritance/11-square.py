b951cf92841b40324504da522bf1aab4cf41d1ae
TonnyMpho <tonnympho1@gmail.com>
2023-07-10 13:55:56 -0700

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module with a class that inherits from
+Rectangle (9-rectangle.py)
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square that inherits from Rectangle """
+    def __init__(self, size):
+        """
+        Instantiation with size
+        validated by integer_validator
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ Returns the area of a Square """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ Square representation """
+        return "[Square] {}/{}".format(self.__size, self.__size)
