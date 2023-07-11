6dcc7aa2319d718a3b9082cc76097baa180cd0ba
TonnyMpho <tonnympho1@gmail.com>
2023-07-11 11:54:17 -0700

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module with a function that writes to a file """
+
+
+def write_file(filename="", text=""):
+    """
+    function that writes a string to a text file (UTF8)
+    and returns the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as w_file:
+        print(w_file.write(text))
