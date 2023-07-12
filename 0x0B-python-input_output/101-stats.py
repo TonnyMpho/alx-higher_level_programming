c54209d8191610370d9c476266c3f1452b7eb856
TonnyMpho <tonnympho1@gmail.com>
2023-07-11 18:56:12 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,7 +3,6 @@
 script that reads stdin line by line and computes metrics
 """
 import sys
-import signal
 
 file_size = 0
 status_code = {}
@@ -19,19 +18,24 @@ def print_stats():
 try:
     for line in sys.stdin:
         count += 1
-        if count % 10 == 0:
-            print_stats()
 
         line = line.strip()
         parts = line.split()
-        if len(parts) >= 9:
+
+        if len(parts) == 9:
             size = int(parts[-1])
             code = parts[-2]
             file_size += size
+
             if code in status_code:
                 status_code[code] += 1
             else:
                 status_code[code] = 1
+
+        if count % 10 == 0:
+            print_stats()
+
+    print_stats()
 
 except KeyboardInterrupt:
     print_stats()
