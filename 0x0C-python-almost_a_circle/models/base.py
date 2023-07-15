cc0b54268d3ef27a4039bd9596d275d43440eaa2
TonnyMpho <tonnympho1@gmail.com>
2023-07-15 08:04:30 -0700

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/nin/python3
+""" the “base” of all other classes in this project """
+
+
+class Base:
+    """
+    the first class
+    base class of all other clasess
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        class constructor - if id is not None, assign the
+        public instance attribute id with this argument value
+        otherwise, increment __nb_objects and assign the new
+        value to the public instance attribute id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
