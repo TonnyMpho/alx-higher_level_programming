ccda2010a2008c56fd5a9477121865c0d0fc1d48
TonnyMpho <tonnympho1@gmail.com>
2023-07-16 11:24:06 -0700

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,9 @@
 #!/usr/nin/python3
 """ the “base” of all other classes in this project """
 import json
+import csv
 import os
+import turtle
 
 
 class Base:
@@ -88,3 +90,71 @@ class Base:
                     instances.append(cls.create(**ins))
                 return instances
         return instances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        class method that serializes in CSV
+        """
+        if list_objs is None:
+            list_objs = []
+
+        filename = cls.__name__ + '.csv'
+        with open(filename, 'w', encoding='utf-8') as f:
+            w = csv.writer(f)
+
+            for obj in list_objs:
+                if cls.__name__ == 'Rectangle':
+                    w.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                if cls.__name__ == 'Square':
+                    w.writerow([obj.id, obj.width, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ class method that deserializes in CSV """
+        filename = cls.__name__ + '.csv'
+
+        instances = []
+        if os.path.isfile(filename):
+            with open(filename, 'r', encoding='utf-8') as f:
+                r = csv.reader(f)
+
+                for row in r:
+                    if cls.__name__ == 'Rectangle':
+                        obj = cls(
+                            int(row[1]), int(row[2]), int(row[3]), int(row[4]),
+                            int(row[0]))
+                    if cls.__name__ == 'Square':
+                        obj = cls(int(row[1]), int(row[2]), int(row[3]), int(row[0]))
+                    instances.append(obj)
+                return instances
+        return instances
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """
+        static method that opens a window and draws all the Rectangles and Squares
+        """
+        window = turtle.Screen()
+        window.bgcolor("white")
+        turtle.speed(2)
+
+        for rect in list_rectangles:
+            turtle.penup()
+            turtle.goto(rect.x, rect.y)
+            turtle.pendown()
+            for _ in range(2):
+                turtle.forward(rect.width)
+                turtle.left(90)
+                turtle.forward(rect.height)
+                turtle.left(90)
+
+        for square in list_squares:
+            turtle.penup()
+            turtle.goto(square.x, square.y)
+            turtle.pendown()
+            for _ in range(4):
+                turtle.forward(square.size)
+                turtle.left(90)
+
+        turtle.done()
