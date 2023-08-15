f6c1bd8a3ae9a7b35f2eeeeb9ef858352bafbaf2
TonnyMpho <tonnympho1@gmail.com>
2023-08-15 16:29:33 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
 -- script that displays the top 3 of cities temperature
 -- during July and August ordered by temperature (descending)
 
-SELECT city, AVG(`value`) AS 'avg_temp' FROM `temperatures` WHERE month = 7 AND month = 8 GROUP BY city ORDER BY `avg_temp` DESC LIMIT 3;
+SELECT city, AVG(`value`) AS 'avg_temp' FROM `temperatures` WHERE MONTH(month) IN (7, 8) GROUP BY city ORDER BY `avg_temp` DESC LIMIT 3;
