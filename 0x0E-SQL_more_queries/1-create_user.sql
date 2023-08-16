4223c7a9f5e4dc0d224a1944c3380432015dcea6
TonnyMpho <tonnympho1@gmail.com>
2023-08-16 12:55:07 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@
 -- If the user user_0d_1 already exists, script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES  ON *.* TO `user_0d_1`@localhost;
+GRANT ALL PRIVILEGES  ON *.* TO `user_0d_1`@localhost;
