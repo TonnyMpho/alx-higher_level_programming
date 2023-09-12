8291ca6d18800b406c2614378f0fa32a76f09da0
TonnyMpho <tonnympho1@gmail.com>
2023-09-11 17:24:36 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,11 +2,18 @@
 // script that prints a square
 
 const n = parseInt(process.argv[2]);
+let squares = '';
 
 if (!isNaN(n)) {
   for (let i = 0; i < n; i++) {
-    console.log('x');
+    for (let j = 0; j < n; j++) {
+      squares += 'X';
+    }
+    if (i < (n - 1)) {
+      squares += '\n';
+    }
   }
+  console.log(squares);
 } else {
   console.log('Missing size');
 }
