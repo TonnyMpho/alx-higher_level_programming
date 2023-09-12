89a04033771c27225e652dd489f9aecbe3b6e3fa
TonnyMpho <tonnympho1@gmail.com>
2023-09-11 18:44:44 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ for (let i = 2; i < process.argv.length; i++) {
   integers.push(parseInt(process.argv[i]));
 }
 
-if (integers.length < 3) {
+if (integers.length < 2) {
   console.log(0);
 } else {
   integers.sort((a, b) => b - a);
