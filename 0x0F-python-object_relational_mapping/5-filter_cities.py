db7eb59ae5adc620e3e2b2a2a44d5936bd81dc84
TonnyMpho <tonnympho1@gmail.com>
2023-09-16 13:43:45 -0700

    Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that takes in the name of a state as an argument
+and lists all cities of that state
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=dbname, port=3306)
+
+    cur = db.cursor()
+    cur.execute("""
+    SELECT cities.name FROM cities
+    JOIN states ON states.id = cities.state_id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """, (state_name,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close
+    db.close
