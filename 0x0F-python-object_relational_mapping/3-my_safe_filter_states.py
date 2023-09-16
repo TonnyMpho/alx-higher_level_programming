92b0eb6051aba57dbcca5e10ff8edcbd63c2ed5a
TonnyMpho <tonnympho1@gmail.com>
2023-09-16 03:29:50 -0700

    Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+import sys
+
+
+def select_state(username, password, dbname, name):
+    """ function to connect and select from the database"""
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=dbname)
+
+    cur = db.cursor()
+    query = ("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+    cur.execute(query, (name,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    name = sys.argv[4]
+
+    select_state(username, password, dbname, name)
