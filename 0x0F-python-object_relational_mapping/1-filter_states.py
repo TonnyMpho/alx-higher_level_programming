ce0eff1e3de389871446c7abd5ece11d88a56554
TonnyMpho <tonnympho1@gmail.com>
2023-09-16 01:25:31 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,20 +12,23 @@ if __name__ == '__main__':
     password = sys.argv[2]
     dbname = sys.argv[3]
 
-    db = MySQLdb.connect(
-            host="localhost",
-            user=username,
-            passwd=password,
-            db=dbname,
-            port=3306)
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                user=username,
+                passwd=password,
+                db=dbname,
+                port=3306)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    rows = cur.fetchall()
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
+        cur.close()
+    except MySQLdb.Error as error:
+        print(error)
+    finally:
+        db.close()
