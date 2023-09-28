7de95cf14d8c75e970e62d89efdc1a78616ca3e7
TonnyMpho <tonnympho1@gmail.com>
2023-09-28 08:17:04 -0700

    Python - Network #0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" | [ "$(cat)" == "200" ] && curl -s "$1"
