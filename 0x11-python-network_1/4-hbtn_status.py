21f6ec1050f59e88c5397c93977b9a36f62f2e37
TonnyMpho <tonnympho1@gmail.com>
2023-09-29 11:24:56 -0700

    Python - Network #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches a URL using requests package"""
+import requests
+
+if __name__ == "__main__":
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(res.text))
+    print('\t- content:', res.text)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+a URL with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    res = requests.post(url, data={'q': sys.argv[1]})
+
+    try:
+        res = res.json()
+
+        if res:
+            print("[{}] {}".format(res['id'], res['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print('Not a valid JSON')
