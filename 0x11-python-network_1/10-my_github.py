40e7ff8dedd8109da93ea542ec6fe32046b6bedc
TonnyMpho <tonnympho1@gmail.com>
2023-09-29 13:56:34 -0700

    Python - Network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    res = requests.get('https://api.github.com/user', auth=(ussername, password))
+    res = requests.get('https://api.github.com/user', auth=(username, password))
 
     if res.status_code == 200:
         user = res.json()
