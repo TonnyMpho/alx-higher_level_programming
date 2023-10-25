c36525b840ba6b4182922820cbcb891d3802ff4a
TonnyMpho <tonnympho1@gmail.com>
2023-10-24 21:17:02 -0700

    JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// script that prints the number of movies where the character
+// “Wedge Antilles” is present.
+
+const request = require('request');
+const url = process.argv[2];
+const character = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(url, (error, res, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const movies = JSON.parse(body);
+    const count = movies.results.filter((movie) => {
+      return movie.characters.includes(character);
+    });
+    console.log(count.length);
+  }
+});
