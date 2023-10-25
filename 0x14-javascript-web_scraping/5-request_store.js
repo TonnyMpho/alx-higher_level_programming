7252e7bd198a719dc5e2ac1b7990b5c248343d74
TonnyMpho <tonnympho1@gmail.com>
2023-10-24 21:26:55 -0700

    JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (error, res, body) => {
+	if (error) {
+		console.log(error);
+	} else {
+		fs.writeFileSync(file, body, 'utf-8', error => {
+			console.log(error);
+		})
+	}
+})
