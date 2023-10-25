3989ad12c525a43809da665045bf57f6fb0a4dda
TonnyMpho <tonnympho1@gmail.com>
2023-10-24 21:29:09 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,11 +7,11 @@ const url = process.argv[2];
 const file = process.argv[3];
 
 request(url, (error, res, body) => {
-	if (error) {
-		console.log(error);
-	} else {
-		fs.writeFileSync(file, body, 'utf-8', error => {
-			console.log(error);
-		})
-	}
-})
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFileSync(file, body, 'utf-8', error => {
+      console.log(error);
+    });
+  }
+});
